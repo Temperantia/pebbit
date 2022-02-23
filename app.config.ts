3e3754cb0ae67a03574723e5e63a04213b1f80ae
Alexandre Du Lorier <adulorier@gmail.com>
2022-02-23 07:42:20 +0100

    patch 38

--- app.config.ts
+++ app.config.ts
@@ -3,9 +3,9 @@ import { ExpoConfig } from "@expo/config-types";
 const config: ExpoConfig = {
   name: "Pebbit",
   slug: "pebbit",
-  version: "0.9.37",
+  version: "0.9.38",
   android: {
-    versionCode: 39,
+    versionCode: 40,
     package: "com.magna_numeris.pebbit",
     googleServicesFile: "./google-services.json",
   },
@@ -48,15 +48,11 @@ const config: ExpoConfig = {
     ],
   ],
   extra: {
-    appEnv: process.env.APP_ENV,
+    appEnv: "process.env.APP_ENV",
     functionsEndpoint:
-      process.env.APP_ENV === "prod"
-        ? "https://us-central1-crypto-2293c.cloudfunctions.net/"
-        : "https://us-central1-pebbit-test.cloudfunctions.net/",
+      'process.env.APP_ENV === "prod" ? "https://us-central1-crypto-2293c.cloudfunctions.net/" : "https://us-central1-pebbit-test.cloudfunctions.net/"',
     firebaseConfig:
-      process.env.APP_ENV === "prod"
-        ? '{"apiKey": "AIzaSyCwTK8s0P6tUqEevJ61Ycut12lrtTJ18Og","authDomain": "crypto-2293c.firebaseapp.com","projectId": "crypto-2293c","storageBucket": "crypto-2293c.appspot.com","messagingSenderId": "876715407348","appId": "1:876715407348:web:3aec09ff57df5a54f52ba1","measurementId": "G-FC15NDED83"}'
-        : '{"apiKey": "AIzaSyDlSg1y7gCtonpo6dHjkZJWNsrDp5_EBUM","authDomain": "pebbit-test.firebaseapp.com","projectId": "pebbit-test","storageBucket": "pebbit-test.appspot.com","messagingSenderId": "156877205943","appId": "1:156877205943:web:5d21f5cf9b5f79bbcef832","measurementId": "G-3JDHFTBFYV"}',
+      'process.env.APP_ENV === "prod" ? \'{"apiKey": "AIzaSyCwTK8s0P6tUqEevJ61Ycut12lrtTJ18Og","authDomain": "crypto-2293c.firebaseapp.com","projectId": "crypto-2293c","storageBucket": "crypto-2293c.appspot.com","messagingSenderId": "876715407348","appId": "1:876715407348:web:3aec09ff57df5a54f52ba1","measurementId": "G-FC15NDED83"}\' : \'{"apiKey": "AIzaSyDlSg1y7gCtonpo6dHjkZJWNsrDp5_EBUM","authDomain": "pebbit-test.firebaseapp.com","projectId": "pebbit-test","storageBucket": "pebbit-test.appspot.com","messagingSenderId": "156877205943","appId": "1:156877205943:web:5d21f5cf9b5f79bbcef832","measurementId": "G-3JDHFTBFYV"}',
   },
 };
 export default config;
